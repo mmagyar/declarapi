a1f72c34017c300133e2c023f90195d4c80fa68d
mmagyar <mmagyar@instructure.com>
2020-03-30 01:17:23 +0200

    add schema and processing

---
+++ src/generate/Generator.spec.ts
@@ -0,0 +1,89 @@
+import { generator } from './Generator'
+
+describe('Generator', () => {
+  it('generates basic example without error', async () => {
+    const result = await generator('./example.json')
+    expect(result).toHaveProperty('type', 'result')
+  })
+
+  it('generates for object single contract input', async () => {
+    const input = {
+      $schema: './schema/singleContractSchema.json',
+      name: 'test',
+      authentication: false,
+      arguments: { myNumber: 'number' },
+      returns: {}
+    }
+
+    const result: any = await generator(input)
+    console.log(JSON.stringify(result, undefined, 2))
+    expect(result.errors).toBeUndefined()
+    expect(result).toStrictEqual({
+      type: 'result',
+      key: 'test',
+      results: [
+        {
+          name: 'test',
+          idFieldName: 'id',
+          authentication: false,
+          method: 'get',
+          arguments: {
+            myNumber: 'number'
+          },
+          returns: {}
+        }
+      ]
+    })
+  })
+
+  it('generates for object crud contract input', async () => {
+    const input = {
+      $schema: './schema/crudContractSchema.json',
+      idFieldName: 'id',
+      name: 'test',
+      authentication: false,
+      dataType: { id: 'string', myNumber: 'number' }
+    }
+
+    const result: any = await generator(input)
+
+    expect(result.errors).toBeUndefined()
+    expect(result).toHaveProperty('type', 'result')
+    expect(result.results).toHaveLength(5)
+  })
+
+  it('rejects unknown $schema', async () => {
+    const input = {
+      $schema: './random.json',
+      name: 'test',
+      authentication: false,
+      arguments: { myNumber: 'number' },
+      returns: {}
+    }
+
+    const result: any = await generator(input)
+    expect(result.errors).toEqual(
+      'Unsupported schema for declaration: ./random.json'
+    )
+  })
+
+  it('rejects invalid type', async () => {
+    const input = {
+      $schema: './schema/singleContractSchema.json',
+      name: 'test',
+      authentication: false,
+      arguments: { myNumber: 'number', invalidProp: 'imaginaryType' },
+      returns: {}
+    }
+
+    const result: any = await generator(input)
+    expect(result.errors).toHaveLength(12)
+    expect(result.errors[0]).toEqual({
+      dataPath: ".arguments['invalidProp']",
+      keyword: 'type',
+      message: 'should be object',
+      params: { type: 'object' },
+      schemaPath: '#/type'
+    })
+  })
+})
