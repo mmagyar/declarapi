2398a028f5c3083b7ef483a609aa7ed93fb2ffdb
Magyar Máté <dev@mmagyar.com>
2020-06-20 20:01:45 +0000

    Project dev settings update

--- src/globalTypes.ts
+++ src/globalTypes.ts
@@ -1,6 +1,4 @@
-import { ObjectType } from "yaschva";
-
+import { ObjectType } from 'yaschva'
 
 export type HttpMethods = 'get' | 'post' | 'put' | 'patch' | 'delete';
 export type SearchTypes = 'textSearch' | 'full' | 'idOnly' | ObjectType;
-
