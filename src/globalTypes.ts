0fb0e24ab7fa881ee1b6e98ac82ea09fa028a195
Magyar Máté <dev@mmagyar.com>
2020-06-21 18:53:22 +0000

    define ContractType inside delcarapi, instead of the generated code

--- src/globalTypes.ts
+++ src/globalTypes.ts
@@ -1,4 +1,25 @@
-import { ObjectType } from 'yaschva'
+import { ObjectType, Validation } from 'yaschva'
 
 export type HttpMethods = 'get' | 'post' | 'put' | 'patch' | 'delete';
 export type SearchTypes = 'textSearch' | 'full' | 'idOnly' | ObjectType;
+
+export type ContractType<T, K> = {
+  name: string
+  authentication: boolean | string[] |
+  {
+    get: boolean | string[]
+    modify: boolean | string[]
+    delete?: boolean | string[]
+  } |
+  {
+    get: boolean | string[]
+    post: boolean | string[]
+    put: boolean | string[]
+    patch: boolean | string[]
+    delete: boolean | string[]
+  }
+  type: 'get' | 'post' | 'put' | 'patch' | 'delete'
+  handle?: (input: T) => Promise<K>
+  arguments: Validation
+  returns: Validation
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 
+export { ContractType } from './globalTypes'
