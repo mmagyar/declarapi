ee3fecb63d18d1bbfca8931d3b7f4fe26b4784ef
Magyar Máté <dev@mmagyar.com>
2020-06-28 08:47:07 +0000

    change elastic export

--- src/generate/elastic.ts
+++ src/generate/elastic.ts
@@ -8,25 +8,25 @@ export type ElasticInputBase = {idField:string, method: HttpMethods, search? :Se
 export type ElasticInputType = ElasticInputBase & (ElasticInputGet | {
   method: 'post' | 'put' | 'patch' | 'delete',
 })
-export const elastic = (driver: Elastic, input: ElasticInputType):string => {
+export const elasticCodeGen = (driver: Elastic, input: ElasticInputType):string => {
   const { index } = driver
   const idField = input.idField
   switch (input.method) {
     case 'get': {
       if (input.search === 'textSearch') {
-        return `input => elasticGet("${index}", input && input.${idField}, input && input.search)`
+        return `input => elastic.get("${index}", input && input.${idField}, input && input.search)`
       } else if (input.search === 'idOnly') {
-        return `input => elasticGet("${index}", input && input.${idField})`
+        return `input => elastic.get("${index}", input && input.${idField})`
       } else if (input.search === 'full') {
         throw new Error('Parametric get not implemented yet')
       }
       throw new Error(`Unsupported automatic elasticsearch methods: ${JSON.stringify(input.search)}`)
     }
-    case 'post': return `input => elasticPost("${index}", input, "${idField}")`
-    case 'patch': return `input => elasticPatch("${index}", input, input.${idField})`
-    case 'put': return `input => elasticPatch("${index}", input, input.${idField})`
-    case 'delete': return `input => elasticDel("${index}", input.${idField})`
+    case 'post': return `input => elastic.post("${index}", input, "${idField}")`
+    case 'patch': return `input => elastic.patch("${index}", input, input.${idField})`
+    case 'put': return `input => elastic.patch("${index}", input, input.${idField})`
+    case 'delete': return `input => elastic.del("${index}", input.${idField})`
   }
 }
 
-export default elastic
+export default elasticCodeGen
