0f2b7930efffeb94fbcc7001739e844d70e2c3c4
Magyar Máté <dev@mmagyar.com>
2020-07-24 22:50:13 +0000

    fix unit test

--- src/runtime/registerRestMethods.spec.ts
+++ src/runtime/registerRestMethods.spec.ts
@@ -103,11 +103,11 @@ describe('registerRestMethods', () => {
       const req = reqMock({ a: 'sadf' }, undefined, undefined, { permissions: ['user', ' moderator'], sub: 'abc' })
 
       await result[0].handler(req, res.chainedMock)
-      expectResult(res, 401, {
-        code: 401,
+      expectResult(res, 403, {
+        code: 403,
         data: { id: undefined },
         errorType: 'unauthorized',
-        errors: ['Only logged in users can do this']
+        errors: ['You don\'t have permission to do this']
       })
     })
 
@@ -117,11 +117,11 @@ describe('registerRestMethods', () => {
       const result = registerRestMethods(data)
       const res = resMock()
       await result[0].handler(reqMock({ a: 'sadf' }), res.chainedMock)
-      expectResult(res, 403, {
-        code: 403,
+      expectResult(res, 401, {
+        code: 401,
         data: { id: undefined },
         errorType: 'unauthorized',
-        errors: ["You don't have permission to do this"]
+        errors: ['Only logged in users can do this']
       })
     })
   })
