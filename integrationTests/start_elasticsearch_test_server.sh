039648bb3c0e90bd08f5126d7db17c0d1a4f5953
Magyar Máté <dev@mmagyar.com>
2020-07-31 20:39:53 +0000

    make integration tests work with elasticserach service

--- integrationTests/start_elasticsearch_test_server.sh
+++ integrationTests/start_elasticsearch_test_server.sh
@@ -4,8 +4,10 @@ set -e
 DIR=$(dirname $(readlink -f $0))
 $DIR/stop_elasticsearch_test_server.sh
 
-
-if [ ! "$(docker ps -q -f name=elastic_test)" ]; then
+if ! command -v docker &> /dev/null; then
+    echo "Docker not installed, trying to start elasticsearch with service"
+    service elasticsearch start
+elif [ ! "$(docker ps -q -f name=elastic_test)" ]; then
   docker pull docker.elastic.co/elasticsearch/elasticsearch:7.8.0
 
   docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name "elastic_test" docker.elastic.co/elasticsearch/elasticsearch:7.8.0
--- integrationTests/stop_elasticsearch_test_server.sh
+++ integrationTests/stop_elasticsearch_test_server.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+if [ $ELASTIC_KEEP ]; then
+    echo "ELASTIC_KEEP option is set, not stopping elasticsearch"
+    exit 0
+fi
+
+if ! command -v docker &> /dev/null; then
+    echo "Docker not installed, trying to stop elasticsearch with service"
+    service elasticsearch stop
+    exit 0
+fi
+
 if [ "$(docker ps -q -f name=elastic_test)" ]; then
-    if [ $ELASTIC_KEEP ]; then
-        echo "ELASTIC_KEEP option is set, not stopping elasticsearch"
-        exit 0
-    fi
 
 
     echo "Stopping and removing docker container named \"elastic_test\""
